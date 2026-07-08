7ab58278fe5c0db3dbf4002652b59c7dc7723a72
Ubuntu <ubuntu@ip-172-31-26-35.ap-southeast-2.compute.internal>
2026-07-08 19:11:23 +0000

    Day 2

---
+++ day02/happy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#This is day02
+
+<<comment
+This is a example of multiple line comment
+comment
+
+name="Md Farhan Kalim"
+
+echo "My name is $name, and the date is $(date)"
+
+echo "Please Enter your username: "
+
+read username
+
+echo "Your name is $username"
+
